fa7bc81026fc69cfdf57012c1fbea9e1908744cc
Kevin Bost <kitokeboo@gmail.com>
2017-11-17 14:38:47 -0800

    Working example with AvalonEdit displaying the code.

---
+++ ShowMeTheXAML.AvalonEdit/TextDocumentValueConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using ICSharpCode.AvalonEdit.Document;
+
+namespace ShowMeTheXAML.AvalonEdit
+{
+    public class TextDocumentValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string @string)
+            {
+                return new TextDocument(@string);
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (value as TextDocument)?.Text;
+        }
+    }
+}
