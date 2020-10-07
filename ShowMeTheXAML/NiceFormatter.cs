01e685f5c8e5499b5e596f3d609f09c1be49c91a
Kevin Bost <kitokeboo@gmail.com>
2020-10-07 14:53:59 -0700

    Adjusting build to include solutions.

--- ShowMeTheXAML/NiceFormatter.cs
+++ ShowMeTheXAML/NiceFormatter.cs
@@ -4,7 +4,6 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using System.Xml;
 using System.Xml.Linq;
 using ShowMeTheXAML.Helpers;
