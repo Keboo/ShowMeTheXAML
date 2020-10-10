539fa6d8e99559044f5910a81f99710b56e20dd2
Kevin B <Keboo@users.noreply.github.com>
2020-10-10 11:45:43 -0700

    Updating examples with latest 2.0.0 preview (#19)

---
+++ Examples/VB.Framework/Application.xaml.vb
@@ -0,0 +1,12 @@
+﻿Imports ShowMeTheXAML
+
+Class Application
+
+    ' Application-level events, such as Startup, Exit, and DispatcherUnhandledException
+    ' can be handled in this file.
+
+    Protected Overrides Sub OnStartup(e As StartupEventArgs)
+        XamlDisplay.Init()
+        MyBase.OnStartup(e)
+    End Sub
+End Class
